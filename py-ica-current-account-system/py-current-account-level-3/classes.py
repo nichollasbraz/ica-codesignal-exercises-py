657f68fdb516e7358f4b056bd3c28f10eef65ada
nichollasbraz <nichollass.fernandes@gmail.com>
2026-09-05 19:08:37 -0300

    feito nível 3

---
+++ py-ica-current-account-system/py-current-account-level-3/classes.py
@@ -0,0 +1,255 @@
+class CheckingAccount():
+    def __init__(self):
+        self.accounts = {}
+        self.withdrawals = {}
+        self.limits = {}
+
+        self.transactions = {}
+        self.transaction_counter = len(self.transactions)
+
+        self.total_deposits = 0
+        self.total_withdrawals = 0
+        
+        self.limit = 0
+
+
+    def create_account(self, account_id: str, timestamp: int, initial_balance: int) -> bool:
+        if account_id not in self.accounts:
+            self.accounts[account_id] = initial_balance
+            self.withdrawals[account_id] = []
+            self.transactions[account_id] = []
+            
+            return True
+        else:
+            return False
+
+
+    def get_balance(self, account_id: str) -> int | None:
+        if account_id not in self.accounts:
+            return None
+        else:
+            return self.accounts[account_id]
+
+
+    def deposit(self, account_id: str, timestamp: int, amount: int) -> int | None:
+        if account_id not in self.accounts:
+            return None
+        else:
+            new_transaction = {
+                "type":"deposit",
+                "amount": amount,
+                "timestamp": timestamp,
+                "balance_after": self.accounts[account_id]
+            }
+
+            self.transactions[account_id].append(new_transaction)
+
+            self.accounts[account_id] += amount
+            self.total_deposits += amount
+            self.transaction_counter += 1
+            
+            return self.accounts[account_id]
+
+
+    def withdraw(self, account_id: str, timestamp: int, amount: int) -> int | None:
+        if account_id not in self.accounts:
+            return None
+        if amount > self.accounts[account_id]:
+            new_withdrawal_none = {
+                        "amount":amount,
+                        "timestamp":timestamp,
+                        "status":"BLOCKED"
+                    }
+            self.withdrawals[account_id].append(new_withdrawal_none)
+
+            return None
+
+        new_transaction = {
+            "type":"withdraw",
+            "amount": amount,
+            "timestamp": timestamp,
+            "balance_after": self.accounts[account_id]
+            }
+                
+        self.transactions[account_id].append(new_transaction)
+        
+        self.accounts[account_id] -= amount
+        self.total_withdrawals += amount
+        self.transaction_counter += 1
+
+
+        new_withdrawal = {
+            "amount":amount,
+            "timestamp":timestamp,
+            "status":"SUCCESS"
+        }
+
+        self.withdrawals[account_id].append(new_withdrawal)
+
+        return self.accounts[account_id]
+
+
+    def get_account_info(self, account_id: str) -> dict | None:
+        if account_id not in self.accounts:
+            return None
+        else:
+            return {
+                "account_id": account_id,
+                "balance": self.accounts[account_id],
+                "total_deposits": self.total_deposits,
+                "total_withdrawals": self.total_withdrawals,
+                "total_transactions": self.transaction_counter 
+            }
+
+    def get_transaction_history(self, account_id: str, limit: int) -> list[dict]:
+        if account_id not in self.accounts:
+            return None
+        if len(self.transactions[account_id]) == 0:
+            return None
+        else:
+            srtHistory = sorted(self.transactions[account_id], key=lambda item: -item["timestamp"])
+
+            return srtHistory[:limit]
+
+
+    def daily_withdrawal_total(self, account_id: str, timestamp: int) -> int:
+        if account_id not in self.accounts:
+            return None
+        else:
+            total = 0
+
+            for transaction in self.transactions[account_id]:
+                if transaction["type"] == "withdraw" and transaction["timestamp"] == timestamp:
+                    total += transaction["amount"]
+
+            return total
+                
+
+    def largest_transaction(self, account_id: str) -> dict | None:
+        if account_id not in self.accounts:
+            return None
+        if len(self.transactions[account_id]) == 0:
+            return None
+        else:
+            biggestAmount = 0
+            biggestType = ""
+            biggestTimestamp = 0
+
+            for transaction in self.transactions[account_id]:   
+                if transaction["amount"] > biggestAmount:
+                    biggestAmount = transaction["amount"]
+                    biggestType = transaction["type"]
+                    biggestTimestamp = transaction["timestamp"]
+
+            return {
+                "type": biggestType,
+                "amount": biggestAmount,
+                "timestamp": biggestTimestamp
+            }
+        
+
+    def set_daily_limit(self, account_id: str, limit: int) -> bool:
+        if account_id not in self.accounts:
+            return False
+        self.limits[account_id] = limit
+
+        return True
+
+
+    def withdraw_with_limit(self, account_id: str, timestamp: int, amount: int) -> bool:
+        if account_id not in self.accounts:
+            return False
+        if account_id not in self.limits:
+            return False
+
+        account_balance = self.accounts[account_id]
+        account_limit = self.limits[account_id]
+
+        if amount > account_balance:
+            blocked = {
+            "amount": amount,
+            "timestamp": timestamp,
+            "reason": "insufficient_funds"
+            }
+
+            self.withdrawals[account_id].append(blocked)
+
+            return False
+
+        today_timestamp = timestamp // 1440
+        total_withdrawn_today = 0
+
+        for transaction in self.transactions[account_id]:
+            if transaction["type"] == "withdraw":
+                timestamps = transaction["timestamp"] // 1440
+                if timestamps == today_timestamp:
+                    total_withdrawn_today += transaction["amount"]
+
+        if total_withdrawn_today + amount > account_limit:
+            blocked = {
+                "amount": amount,
+                "timestamp": timestamp,
+                "reason": "daily_limit_exceeded"
+            }
+
+            self.withdrawals[account_id].append(blocked)
+
+            return False
+        else:
+            self.accounts[account_id] -= amount
+
+            new_withdraw_limit = {
+                "type": "withdraw",
+                "timestamp": timestamp,
+                "status": "SUCCESS"
+            }
+
+            self.withdrawals[account_id].append(new_withdraw_limit)
+
+            new_transaction_limit = {
+            "type": "withdraw",
+            "amount": amount,
+            "timestamp": timestamp,
+            "balance_after": self.accounts[account_id]
+            }
+
+            self.transactions[account_id].append(new_transaction_limit)
+
+            self.total_withdrawals += amount
+
+            return True
+
+
+    def get_blocked_transactions(self, account_id: str) -> list[dict]:
+        if account_id not in self.accounts:
+            return None
+        else:
+            blocked = []
+
+            for withdraw in self.withdrawals[account_id]:
+                if "reason" in withdraw:
+                    blocked.append(withdraw)
+                elif withdraw["status"] == 'BLOCKED':
+                    blocked.append(withdraw)
+
+            return blocked
+
+        
+c = CheckingAccount()
+print(c.create_account("malagoli", 0, 2000))
+print(c.get_balance("malagoli"))
+print(c.deposit("malagoli", 30, 200))
+print(c.get_account_info("malagoli"))
+print(c.withdraw("malagoli", 22, 400))
+print(c.get_account_info("malagoli"))
+print(c.withdraw("malagoli", 22, 200))
+print(c.get_account_info("malagoli"))
+print(c.withdraw("malagoli", 12, 20000))
+print(c.get_account_info("malagoli"))
+print(c.get_transaction_history("malagoli", 22))
+print(c.daily_withdrawal_total("malagoli", 22))
+print(c.largest_transaction("malagoli"))
+print(c.set_daily_limit("malagoli", 3500))
+print(c.withdraw_with_limit("malagoli", 10, 50))
+print(c.withdraw_with_limit("malagoli", 10, 100))
+print(c.get_blocked_transactions("malagoli"))
