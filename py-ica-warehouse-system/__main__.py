4664ea5c88a30b1fed25d7cf91fc7dd2220dcc45
nichollasbraz <nichollass.fernandes@gmail.com>
2026-09-21 21:47:24 -0300

    nova resolução do ICA(2)

--- py-ica-warehouse-system/__main__.py
+++ py-ica-warehouse-system/__main__.py
@@ -82,15 +82,74 @@ class Warehouse():
                 "total_sold": total_sold
             }
 
+
+    def get_sales_history(self, product_id: str = "") -> list[dict]:
+        if product_id is None or product_id == "":
+            return None
+
+        if product_id not in self.products:
+            return self.sales
+
+        total_sales = []
+
+        for sale in self.sales:
+            if sale["product_id"] == product_id:
+                total_sales.append(sale)
+
+        sorted_sales = sorted(total_sales, key=lambda item: -item["timestamp"])
+            
+        return sorted_sales
+
+
+    def best_seller(self) -> str | None:
+        if len(self.products) == 0:
+            return None
+        if len(self.sales) == 0:
+            return None
+        else:
+            total_sales = {}
+
+            for sale in self.sales:
+                product_id = sale["product_id"]
+                quantity = sale["quantity"]
+
+                if product_id not in total_sales:
+                    total_sales[product_id] = quantity
+                else:
+                    total_sales[product_id] += quantity
+
+            best_product = max(total_sales, key=lambda p: total_sales[p])
+
+        return best_product
+
+
+    def total_revenue(self) -> int:
+        if len(self.products) == 0 or len(self.sales) == 0:
+            return 0
+        else:
+            total_amount = 0
+
+            for sale in self.sales:
+                product_id = sale["product_id"]
+                quantity = sale["quantity"]
+                price = self.products[product_id]["price"]
+
+                total_amount += quantity * price
+
+            return total_amount
+
+
 p1 = Warehouse()
 print(p1.create_product("Mouse", "Mouse Logitech", 25))
+print(p1.create_product("Monitor", "Monitor Asus", 11))
 print(p1.__dict__)
-print(p1.add_stock("Mouse", 0))
 print(p1.add_stock("Mouse", 15))
+print(p1.add_stock("Monitor", 41))
 print(p1.__dict__)
-print(p1.remove_stock("Mouse", 20, 11))
-print(p1.remove_stock("Mouse", 11, 12))
-print(p1.sales)
-print(p1.remove_stock("Mouse", 3, 13))
-print(p1.sales)
+print(p1.remove_stock("Mouse", 12, 11))
+print(p1.remove_stock("Monitor", 6, 22))
+print(p1.remove_stock("Mouse", 3, 15))
 print(p1.get_product_info("Mouse"))
+print(p1.get_sales_history("Mouse"))
+print(p1.best_seller())
+print(p1.total_revenue())
